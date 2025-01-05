658b55f6bcd8165db1aa1cd09a8fbb8e735377a0
Morganero <morganero1@gmail.com>
2025-01-05 16:46:15 -0600

    removed some comment and unused code

--- src/utils/calcYears.js
+++ src/utils/calcYears.js
@@ -1,14 +1,12 @@
 function calculateYears(startDate) {
   const [startMonth, startYear] = startDate.split('/').map(Number);
-  const startDateObj = new Date(startYear, startMonth - 1); // Month is 0-indexed in JavaScript Date
 
   const currentDate = new Date();
   const currentYear = currentDate.getFullYear();
-  const currentMonth = currentDate.getMonth() + 1; // Month is 0-indexed in JavaScript Date
+  const currentMonth = currentDate.getMonth() + 1;
 
   let yearsDifference = currentYear - startYear;
 
-  // Adjust the difference if the current month is before the start month
   if (currentMonth < startMonth) {
     yearsDifference--;
   }
