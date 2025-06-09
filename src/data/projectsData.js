4a074b44ff8a08b78b7202f0ad9cec7c61c52aae
Morganero <morganero1@gmail.com>
2025-06-09 10:01:57 -0500

    added new mythguard project

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -17,8 +17,19 @@ const projectsData = [
     imageURL:
       '/portfolio-images/Screen%20Shot%202024-12-28%20at%2018.36.59.png?updatedAt=1735432672199',
   },
+
   {
     id: 3,
+    title: 'Mythguard',
+    techList: ['NextJs', 'TypeScript', 'Tailwind'],
+    viewProjectLink: 'https://mythguard-3.vercel.app/',
+    viewCodeLink: 'https://github.com/MorganEro/Mythguard-3',
+    imageURL:
+      '/portfolio-images/Screen%20Shot%202025-06-09%20at%2009.54.29.png?updatedAt=1749481059126',
+  },
+
+  {
+    id: 4,
     title: 'E-commerce Product Page',
     techList: ['React', 'Bootstrap'],
     viewProjectLink: 'https://morganero.github.io/ecommerce-product-page/',
