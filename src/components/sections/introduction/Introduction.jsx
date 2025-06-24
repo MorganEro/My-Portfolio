2900d2fd03808e2cce26016a6a677761859aae3e
Morganero <morganero1@gmail.com>
2025-06-24 14:31:00 -0500

    changed background for intro

--- src/components/sections/introduction/Introduction.jsx
+++ src/components/sections/introduction/Introduction.jsx
@@ -1,14 +1,13 @@
 import ContactButton from '../../ui/buttons/ContactButton';
-import RisingArrows from '../../ui/RisingArrows';
 
 function Introduction() {
   return (
     <section
       id="Introduction"
       className="section-introduction d-flex flex-column align-items-center justify-content-center text-center px-4 gap-2 pt-5 px-1 px-md-3 px-lg-5 py-5">
-      <div className="container">
+      <div className="container section-introduction__container">
         <h1
-          className="section-introduction__heading display-1 pb-4 text-light-100"
+          className="section-introduction__heading display-1 text-light-100"
           id="intro-title">
           Nice to meet you! I'm{' '}
           <span className="text-dark-300">Morgan&nbsp;Ero</span>
@@ -28,7 +27,6 @@ function Introduction() {
         </p>
         <ContactButton className="animated-container" />
       </div>
-      <RisingArrows />
     </section>
   );
 }
