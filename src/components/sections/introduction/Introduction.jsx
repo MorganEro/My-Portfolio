5ae349622409a3c9594969ed21e27cd8290036e6
Morganero <morganero1@gmail.com>
2025-06-25 14:30:31 -0500

    made intro more responsive and balanced

--- src/components/sections/introduction/Introduction.jsx
+++ src/components/sections/introduction/Introduction.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-scroll';
 import ContactButton from '../../ui/buttons/ContactButton';
 
 function Introduction() {
@@ -6,26 +7,33 @@ function Introduction() {
       id="Introduction"
       className="section-introduction d-flex flex-column align-items-center justify-content-center text-center px-4 gap-2 pt-5 px-1 px-md-3 px-lg-5 py-5">
       <div className="container section-introduction__container">
-        <h1
-          className="section-introduction__heading display-1 text-light-100"
-          id="intro-title">
-          Nice to meet you! I'm{' '}
-          <span className="text-dark-300">Morgan&nbsp;Ero</span>
+        <h1 className="section-introduction__heading1 display-3 text-light-300">
+          Hello! <span className="heading1-span">I'm</span>
         </h1>
+        <h1 className="section-introduction__heading2 me-text-primary ">
+          <Link
+            to="Contact"
+            smooth={true}
+            duration={500}
+            aria-label="Contact me">
+            <span className="heading2-span"> I'm</span>
+            <span className="first-name px-1">Morgan</span>
+            <span className="last-name  px-1">Ero</span>
+          </Link>
+        </h1>
+        <h1
+          className="section-introduction__heading3 display-3 text-light-300"
+          id="intro-title">
+          Welcome to my Portfolio!
+        </h1>
+
         <p
-          className="section-introduction__description text-lg text-light-100"
+          className="section-introduction__description text-lg text-light-300"
           id="intro-description">
-          I'm a <strong className="text-dark-300">Frontend Developer</strong>{' '}
-          with a passion for creating beautiful and functional web applications.
-          I love turning complex problems into simple, elegant solutions while
-          <em className="text-dark-300">
-            {' '}
-            continuously striving to grow my skills
-          </em>{' '}
-          and stay updated with the latest industry trends. Let's build
-          something amazing together!
+          I'm a Frontend Developer with a passion for creating responsive and
+          functional web applications. I am constantly striving to grow my
+          skills and stay updated with the latest industry trends.
         </p>
-        <ContactButton className="animated-container" />
       </div>
     </section>
   );
